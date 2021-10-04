2ef77004b5f47da7937496cd4a673746d984ad7e
DruboSaptapan <saptapan2017@gmail.com>
2021-10-04 10:50:13 +0600

    first-commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+           <p>Copyright 2021, All Right Reserved</p>
+        </div>
+    );
+};
+
+export default Footer;
